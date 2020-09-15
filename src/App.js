de3a4a3b51fcc9f7e3e071dc4e4f87efaede7bb0
bryanbilek <bbilek35@gmail.com>
2020-09-15 18:51:49 -0500

    brought in bitmoji for avatar

--- src/App.js
+++ src/App.js
@@ -6,25 +6,22 @@ import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-    <div style={{ height: "300px", position: "relative" }}>
-      <Layout
-        style={{
-          background:
-            "url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover",
-        }}
-      >
-        <Header transparent title="Bryan Bilek" style={{ color: "white" }}>
-          <Navigation>
-            <Link to="/about">About</Link>
-            <Link to="/resume">Resume</Link>
-            <Link to="/projects">Projects</Link>
-            <Link to="/contact">Contact</Link>
-          </Navigation>
-        </Header>
-        <Content>
-          <Dashboard />
-        </Content>
-      </Layout>
+    <div className="app">
+      <div style={{ height: "300px", position: "relative" }}>
+        <Layout>
+          <Header transparent title="Bryan Bilek" style={{ color: "white" }}>
+            <Navigation>
+              <Link to="/about">About</Link>
+              <Link to="/resume">Resume</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
+            </Navigation>
+          </Header>
+          <Content>
+            <Dashboard />
+          </Content>
+        </Layout>
+      </div>
     </div>
   );
 }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Bitmoji from "../images/bryan-bitmoji.png";
 
 export default function HomePage() {
   return (
-    <div>
-      <p>HomePage</p>
+    <div className="home-page">
+      <img src={Bitmoji} alt="bitmoji" className="bitmoji" />
     </div>
   );
 }
